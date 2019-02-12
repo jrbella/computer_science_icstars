0c9b08c80d0a0dea22116032478867b8bf04758a
jeffrey bella <jbella@icstars.org>
2019-02-12 16:10:09 -0600

    Updated powershell script that moves remote directories

--- migrate_files.ps1
+++ migrate_files.ps1
@@ -1,2 +1,12 @@
+#Moves files from projec folder  to git repo folder
 
-$home_path = Get-Location
+
+$home_path = "D:\projects"
+$migration_path = "D:\computer_science_icstars"
+
+function migrate-files ($home_path, $migration_path) {
+    if((test-path $home_path) -and (test-path $migration_path)){
+        copy-item -Path $home_path -Destination $migration_path -Recurse -Force
+    }
+
+}
